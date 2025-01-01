423ceb0cb87a5fe65b88417e6180714c92f2a9fe
Gotorishh96 <23cse101@abit.edu.in>
2025-01-01 23:15:14 +0530

    NAME

---
+++ Name.java
@@ -0,0 +1,6 @@
+public class Name { 
+    public static void main(String[] args) {
+        // Print a greeting message to the console
+        System.out.println("Hello Rishikanta Panda ...! Welcome to the world of JAVA..!!");
+    } 
+}
