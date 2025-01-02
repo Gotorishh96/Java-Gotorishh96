e9ee661968a8bd555faf2d05dee5f789a8c19f9d
Gotorishh96 <23cse101@abit.edu.in>
2025-01-02 22:43:41 +0530

    BIODATA

---
+++ Rishi.java
@@ -0,0 +1,10 @@
+public class Rishi{
+	public static void main(String args[]){
+
+		System.out.println("Rishikanta Panda");
+		System.out.println("Computer Science and Engineering(CSE)");
+		System.out.println("2301206101");
+		System.out.println("Ajay Binay Institute Of Technology(ABIT)");
+		
+		}
+	}
