58552dd00601d8518872cb7e13a8eddf9c754e6c
Gotorishh96 <23cse101@abit.edu.in>
2025-01-02 22:46:17 +0530

    ARITHMETIC OPERATIONS

---
+++ ArithmeticOperation.java
@@ -0,0 +1,18 @@
+public class ArithmeticOperation{
+	public static void main(String args[]){
+		int a,b;
+		a = 5;
+		b = 2;
+			int sum = a + b;
+			int sub = a - b;
+			int mul = a * b;
+			int div = a / b;
+			int mod = a % b;
+
+				System.out.println("Addition is :" + sum);
+				System.out.println("Subtraction is :" + sub);
+				System.out.println("Multiplication is :" + mul);
+				System.out.println("Division is :" + div);
+				System.out.println("Remainder is :" + mod);
+			}
+	}
