d4409b0e61caf315e8df2ed8e658436ac97161c3
Gotorishh96 <23cse101@abit.edu.in>
2025-01-03 22:47:17 +0530

    Area And Circumference Of Circle

---
+++ Circle.java
@@ -0,0 +1,11 @@
+public class Circle{
+	public static void main(String args[]){
+		double r = 4.0;
+		double area = 0.0;
+		double circumference = 0.0;
+			area = 3.14 * r * r;
+			circumference = 2 * 3.14 * r;
+				System.out.println("Area of the Circle = " + area);
+				System.out.println("Circumference of the circle = " + circumference);
+			}
+	}
