c012e2b44f63d7f2d738749dd58e84d1312253fe
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-04 15:48:11 +0530

    Area And Perimeter Of Rectangle

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+public class Rectangle{
+	public static void main(String args[]){
+		double l , b;
+		double area = 0.0;
+		double perimeter = 0.0;
+			l = 6.0;
+			b = 5.0;
+			area = l * b;
+			perimeter = 2 * (l + b);
+				System.out.println("Area of the Rectangle = " + area);
+				System.out.println("Perimeter of the Rectangle = " + perimeter);
+			}
+	}
