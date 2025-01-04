4a9b11d0d7bbab8efc16bee46b0fc6031ac3113a
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-04 15:49:33 +0530

    Area And Perimeter Of Square

---
+++ Square.java
@@ -0,0 +1,14 @@
+public class Square{
+	public static void main(String args[]){
+		double l , b , r;
+		double area = 0.0;
+		double perimeter = 0.0;
+			l = 6.0;
+			b = 5.0;
+			r = 4.0;
+			area = r * r;
+			perimeter = 4 * r;
+				System.out.println("Area of the Square = " + area);
+				System.out.println("Perimeter of the Square = " + perimeter);
+			}
+	}
