640fa3978c05e2d291a8c765d5f6da2af6ac4906
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-05 23:55:24 +0530

    Simple Interest

---
+++ SimpleInterest.java
@@ -0,0 +1,11 @@
+public class SimpleInterest{
+	public static void main(String args[]){
+		double p , r , t;
+		double si;
+			p = 2000.00;
+			r = 12.5;
+			t = 5.0;
+			si = (p * r * t) / 100 ;
+				System.out.println("Simple Interest of the Money is = " + si);
+		}
+	}
