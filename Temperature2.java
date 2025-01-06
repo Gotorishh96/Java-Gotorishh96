301b1150e0c1c8a0b377edab036280f6d8047ca3
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-06 15:27:50 +0530

    Fahrenheit To Celsius

---
+++ Temperature2.java
@@ -0,0 +1,8 @@
+public class Temperature2{
+	public static void main(String args[]){
+		double c , f;
+			f = 125.5;
+			c = (f - 32) * 0.55;
+				System.out.println("The Temperature from Fahrenheit to Celsius is = " + c);
+		}
+	}
