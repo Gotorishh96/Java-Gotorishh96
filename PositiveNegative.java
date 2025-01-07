bc50aebf8c1d25e5c76de6a7bd78941c4bf60ef6
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-07 18:38:24 +0530

    Positive And Negative Number

---
+++ PositiveNegative.java
@@ -0,0 +1,12 @@
+public class PositiveNegative{
+	public static void main(String args[]){
+		int n ;
+		n = 10;
+			if ( n > 0 ){
+				System.out.println("The Number is Positive Number..!");
+			}
+			 else {
+				System.out.println("The Number is Negative Number..!");
+			}
+		}
+	}
