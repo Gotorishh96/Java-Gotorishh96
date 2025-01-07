ec2fdbd015f8fb0f0dd65fbe1025663438c4baee
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-07 18:39:41 +0530

    Even And Odd Number

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd{
+	public static void main(String args[]){
+		int n = 4 ;
+			if ( n % 2 == 0 ){
+				System.out.println("The Number is Even Number..!");
+			}
+			 else {
+				System.out.println("The Number is Odd Number..!");
+			}
+		}
+	}
