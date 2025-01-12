6b192e172a0404fc8000f16f6787bee0e08c32d3
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-12 10:09:54 +0530

    Area And Circumference Of Circle using Scanner Class

---
+++ CircleSC.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CircleSC{
+	public static void main(String args[]){
+		double r , area , circumference;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter any value of the Radius");
+			r = sc.nextDouble();
+			System.out.println("Area of the Circle = + " 3.14 * r * r);
+			System.out.println("Circumference of the Circle = + " 2 * 3.14 * r);
+		}
+	}
