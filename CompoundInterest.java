abecb4b6f504848d94a5f72c87af965346db44a3
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-18 23:56:39 +0530

    Update CompoundInterest.java

--- CompoundInterest.java
+++ CompoundInterest.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class CompoundInterest {
   public static void main(String[] args) {
 
-    // create an object of Scanner class
+     // create an object of Scanner class
     Scanner input = new Scanner(System.in);
 
     // take input from users
