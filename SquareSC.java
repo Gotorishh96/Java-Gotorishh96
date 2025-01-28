2c378a8b8c1b1e38bdb56eb0d41725ccea6cc8cc
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-28 21:15:37 +0530

    Area And Perimeter Of Square using Scanner Class

---
+++ SquareSC.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class SquareSC{
+	public static void main(String args[]){
+		double r;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter any value of the Radius ");
+			r = sc.nextDouble();
+			System.out.println("Area of the Rectangle = " + r * r);
+			System.out.println("Perimeter of the Rectangle = " + 4 * r);
+		}
+	}
