db23176f49d31b629199a777adedd9831a890801
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-29 21:34:36 +0530

    Pattern Programming

---
+++ Pattern4.java
@@ -0,0 +1,15 @@
+public class Pattern4{
+    public static void main(String args[]){
+        int i , j , n;
+        n = 4;
+        for( i = 1 ; i <= n ; i++ ){
+            for( j = 1 ; j <= n-i ; j++ ){
+                System.out.print( " " );
+            }
+             for( j = 1 ; j <= ( 2 * i - 1 ) ; j++){
+                 System.out.print( "*" );
+             }
+            System.out.println( );
+        }
+    }
+}
