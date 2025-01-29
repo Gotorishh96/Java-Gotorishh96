88d8f4adf8862c822ccf187b6a8c275319ea7924
Rishikanta Panda <23cse101@abit.edu.in>
2025-01-29 21:27:40 +0530

    Pattern Programming

---
+++ Pattern3.java
@@ -0,0 +1,14 @@
+public class Pattern3{
+    public static void main(String args[]){
+        int i , j , k;
+        for( i = 1 ; i <= 4 ; i++ ){
+            for( k = 2 ; k <= i ; k++){
+                System.out.print( " " );
+            }
+             for( j = 4 ; j >= i ; j--){
+                 System.out.print( "*" );
+             }
+            System.out.print( "\n" );
+        }
+    }
+}
